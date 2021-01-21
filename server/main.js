206ad0c9a61bdd5e8279c5364c60897a621a2ed1
SeriousSpigol <spig1999@gmail.com>
2021-01-21 17:38:20 +0100

    Amqp

--- server/main.js
+++ server/main.js
@@ -1,47 +1,61 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const mysql = require("mysql");
-const mqtt = require("mqtt");
-var client = mqtt.connect('mqtt://192.168.102.21');
-var connection = mysql.createPool({
+const amqp = require("amqplib/callback_api");
+var sql = mysql.createPool({
     connectionLimit: 10,
     host: 'localhost',
     user: 'root',
     password: 'Vmware1!',
     database: 'IOTProtocols'
 });
-client.on('connect', function () {
-    client.subscribe('scooters/#');
-    console.log("Service on...");
-});
-client.on('message', function (topic, message) {
-    var temp = topic.split("/");
-    var detection = new Detection();
-    detection.scooterid = temp[1];
-    detection.readingdate = new Date();
-    switch (temp[2]) {
-        case "battery":
-            detection.battery = parseInt("" + message);
-            break;
-        case "speed":
-            detection.speed = parseInt("" + message);
-            break;
-        case "position":
-            var valuesTmp = "" + message;
-            var values = valuesTmp.split(" ");
-            detection.lat = parseFloat(values[0]);
-            detection.lon = parseFloat(values[1]);
-            break;
-        case "status":
-            detection.status = "" + message;
-            break;
-        default:
-            break;
+var connectionString = "amqps://bzsuiemn:suzKkZg1O5lH71TFtuBjtBxret2cp6oY@bonobo.rmq.cloudamqp.com/bzsuiemn";
+var queue = 'CantonQueue';
+amqp.connect(connectionString, (error0, connection) => {
+    if (error0) {
+        throw error0;
     }
-    console.log("Data: " + message);
-    connection.query('INSERT INTO detections SET ?;', [detection], function (error, results, fields) {
-        if (!error)
-            console.log("Save successful");
+    connection.createChannel((error1, channel) => {
+        if (error1) {
+            throw error1;
+        }
+        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+        channel.consume(queue, (msg) => {
+            var detection = new Detection();
+            var temp = msg.fields.routingKey.split(".");
+            detection.scooterid = temp[1];
+            var message = msg.content;
+            detection.readingdate = new Date();
+            switch (temp[2]) {
+                case "Battery":
+                    detection.battery = parseInt("" + message);
+                    break;
+                case "Speed":
+                    detection.speed = parseInt("" + message);
+                    break;
+                case "Position":
+                    var valuesTmp = "" + message;
+                    var values = valuesTmp.split(" ");
+                    detection.lat = parseFloat(values[0]);
+                    detection.lon = parseFloat(values[1]);
+                    console.log(values[0]);
+                    console.log(values[1]);
+                    console.log(detection.lat);
+                    console.log(detection.lon);
+                    break;
+                case "Status":
+                    detection.status = "" + message;
+                    break;
+                default:
+                    break;
+            }
+            sql.query('INSERT INTO detctions SET ?;', [detection], function (error, results, fields) {
+                if (error)
+                    console.log(error);
+            });
+        }, {
+            noAck: true
+        });
     });
 });
 class Detection {
--- server/main.ts
+++ server/main.ts
@@ -1,9 +1,7 @@
 import * as mysql from "mysql";
-import * as mqtt from "mqtt";
+import * as amqp from "amqplib/callback_api";
 
-var client  = mqtt.connect('mqtt://192.168.102.21');
-
-var connection = mysql.createPool({
+var sql = mysql.createPool({
     connectionLimit: 10,
     host: 'localhost',
     user: 'root',
@@ -11,41 +9,63 @@ var connection = mysql.createPool({
     database: 'IOTProtocols'
 });
 
-client.on('connect', function () {
-    client.subscribe('scooters/#');
-    console.log("Service on...");
-  })
+var connectionString = "amqps://bzsuiemn:suzKkZg1O5lH71TFtuBjtBxret2cp6oY@bonobo.rmq.cloudamqp.com/bzsuiemn";
+var queue = 'CantonQueue';
 
-  client.on('message', function(topic, message){
-      var temp = topic.split("/");
-      var detection = new Detection();
-      detection.scooterid = temp[1];
-      detection.readingdate = new Date();
-      switch (temp[2]) {
-          case "battery":
-                detection.battery = parseInt("" + message);
-            break;
-            case "speed":  
-                detection.speed = parseInt("" + message);
-            break;
-            case "position": 
-                var valuesTmp = "" + message;
-                var values = valuesTmp.split(" ");
-                detection.lat = parseFloat(values[0]);
-                detection.lon = parseFloat(values[1]);
-            break;
-            case "status": 
-                detection.status = "" + message;
-            break;
-          default:
-            break;
-      }
-      console.log("Data: " + message);
-      connection.query('INSERT INTO detections SET ?;', [detection], function (error, results, fields) {
-          if(!error)
-          console.log("Save successful");
-      });
-  })
+amqp.connect(connectionString, (error0, connection) => {
+    if (error0) {
+        throw error0;
+    }
+    connection.createChannel( (error1, channel) => {
+        if (error1) {
+            throw error1;
+        }
+
+        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+
+        channel.consume(queue, (msg) => {
+            var detection = new Detection();
+            var temp = msg.fields.routingKey.split(".");
+            detection.scooterid = temp[1];
+            var message = msg.content;
+            detection.readingdate = new Date();
+            switch (temp[2]) {
+                case "Battery":
+                    detection.battery = parseInt("" + message);
+                    break;
+                case "Speed":
+                    detection.speed = parseInt("" + message);
+                    break;
+                case "Position":
+                    var valuesTmp = "" + message;
+                    var values = valuesTmp.split(" ");
+                    detection.lat = parseFloat(values[0]);
+
+
+                    
+                    detection.lon = parseFloat(values[1]);
+                    console.log(values[0]);
+                    console.log(values[1]);
+                    console.log(detection.lat);
+                    console.log(detection.lon);
+                    break;
+                case "Status":
+                    detection.status = "" + message;
+                    break;
+                default:
+                    break;
+            }
+            
+            sql.query('INSERT INTO detctions SET ?;', [detection], function (error, results, fields) {
+                if (error)
+                    console.log(error);
+            });
+           
+        }, {
+            noAck: true
+        });
+    });
+});
 
 class Detection {
     constructor(
@@ -58,3 +78,5 @@ class Detection {
         public readingdate?: Date
     ) { }
 }
+
+
